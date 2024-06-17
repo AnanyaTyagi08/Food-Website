96992a35e6a29d58a44007fabc7886e6a3918faa
wowza82 <at899@njit.edu>
2024-06-17 18:48:31 -0400

    Create Websites

---
+++ Websites/jain_meals_website.php
@@ -0,0 +1,19 @@
+<html>
+    <header>
+        <title>What To Eat | Jain Edition</title>
+        <link rel = "icon" href = "foodCartoon.png" type = "image/png" />
+        <!--<link rel = "stylesheet" type = "text/css" href ="CSS file name with extension">-->
+    </header>
+    <body>
+        <div class = "header">
+            <h1 style = "color black">Meal Prep Picker</h1>
+            <h2 class = "motto" style = "color grey"><em>Jain Friendly Edition</em></h3>
+        </div>
+
+        <p>Generates 3 meals for an average of a day's worth of eating catering to different dietary plans<p>
+
+    </body>
+    <footer>
+
+    </footer>
+</html>
